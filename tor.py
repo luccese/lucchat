86026701a4762174c3056748f5e5b1c4bfd64e4e
luccese <bartorex11m@interia.eu>
2020-08-04 12:09:37 +0200

    Update tor.py

--- tor.py
+++ tor.py
@@ -4,22 +4,26 @@ from bs4 import BeautifulSoup
 import zipfile
 import psutil
 
+
 def installTor():
 	print("Wait, I'm downloading tor.zip")
-	urllib.request.urlretrieve("https://torproject.org" + BeautifulSoup(urllib.request.urlopen('https://torproject.org/download/tor/').read(), 'html.parser').find("a", class_="downloadLink")["href"], filename="tor.zip")
+	urllib.request.urlretrieve("https://torproject.org" + BeautifulSoup(urllib.request.urlopen('https://torproject.org/download/tor/').read(),'html.parser').find("a", class_="downloadLink")["href"],filename="tor.zip")
 	with zipfile.ZipFile("tor.zip", 'r') as zip_ref: zip_ref.extractall("TOR")
 	print("Unziping...")
-	with open("TOR\\Tor\\torrc", "a") as torrc: torrc.write("HiddenServiceDir " + os.getcwd() + "\nHiddenServicePort 5555 127.0.0.1:5555")
+	with open("TOR\\Tor\\torrc", "a") as torrc:
+		torrc.write("HiddenServiceDir " + os.getcwd() + "\nHiddenServicePort 5555 127.0.0.1:5555")
+		torrc.close()
 	print("torrc was created, tor installed.")
 	os.remove('tor.zip')
-	checkInstall()
+
 
 def checkInstall():
 	if os.path.isfile("TOR\\Tor\\torrc"):
 		print("tor installed.")
 		return True
 	else:
-		installTor()
+		return False
+
 
 def checkRunning():
 	for p in psutil.process_iter(attrs=['pid', 'name']):
@@ -27,6 +31,18 @@ def checkRunning():
 			print("Tor already running")
 			return True
 
+
 def runTor():
 	print("Running tor.exe")
-	os.popen("TOR\\Tor\\tor.exe")
+	os.popen("TOR\\Tor\\tor.exe -f TOR\\Tor\\torrc")
+
+
+def hostnameTor():
+	try:
+		with open("hostname", "r") as r:
+			hostname = r.read()
+			r.close()
+		return hostname
+	except Exception as ex:
+		return str(ex)
+
